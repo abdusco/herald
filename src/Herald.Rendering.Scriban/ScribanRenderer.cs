10863783d71c6f1932c4a124956068fef08d6c5c
Abdussamet Kocak <abdus@abdus.dev>
2020-11-26 20:08:21 +0300

    Add Scriban renderer

---
+++ src/Herald.Rendering.Scriban/ScribanRenderer.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Scriban;
+
+namespace Herald.Rendering.Scriban
+{
+    public class ScribanRenderer : IRenderer
+    {
+        public async Task<string> RenderAsync<T>(
+            string template, T model,
+            CancellationToken cancellationToken = default
+        ) =>
+            await Template.Parse(template)
+                .RenderAsync(model)
+                .ConfigureAwait(false);
+    }
+}
---
+++ src/Herald.Tests/RendererTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Herald.Rendering.Scriban;
+using Xunit;
+
+namespace Herald.Tests
+{
+    public class RendererTests
+    {
+        [Fact]
+        public async Task CanRenderTemplate()
+        {
+            const string template = @"hello {{who}}";
+            var r = new ScribanRenderer();
+
+            var rendered = await r.RenderAsync(template, new
+            {
+                Who = "world"
+            });
+
+            Assert.Equal("hello world", rendered);
+        }
+
+        [Fact]
+        public async Task CanRenderLists()
+        {
+            var r = new ScribanRenderer();
+            const string template = @"{{- for item in list -}}{{ item }}{{- end -}}";
+            var model = new
+            {
+                List = new[] {"a", "b", "c"}
+            };
+
+            var rendered = await r.RenderAsync(template, model);
+
+            Assert.Equal(@"abc", rendered, ignoreWhiteSpaceDifferences: true, ignoreLineEndingDifferences: true);
+        }
+    }
+}
